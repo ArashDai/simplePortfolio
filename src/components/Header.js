19110140450ff4a3320e4c24d99ce8cc126a458f
geryegan <gery.egan@gmail.com>
2015-12-06 18:31:03 -0800

    minor stuff

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,47 +1,11 @@
 var React = require('react');
-<<<<<<< HEAD
-var Router = require('react-router')
-var Link = require('react-router').Link;
-=======
 var Router = require('react-router');
-var Link = Router.Link;
->>>>>>> 3964c37304612cfc314b4fcac0bde8b408d8b923
+var Link = require('react-router').Link;
+
 var Route = Router.Route;
 
 var Header = React.createClass({
 
-<<<<<<< HEAD
-
-render: function(){
-	return(
-		<nav className="navbar navbar-default" role="navigation" id="mainNavbar">
-			
-			<div className="navbar-header">
-				<button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-					<span className="sr-only">Toggle navigation</span>
-					<span className="icon-bar"></span>
-					<span className="icon-bar"></span>
-					<span className="icon-bar"></span>
-				</button>
-				<Link className="navbar-brand" to="/">Casa Home Staging</Link>
-			</div>
-		
-			
-			<div className="collapse navbar-collapse navbar-ex1-collapse">
-				<ul className="nav navbar-nav">
-					<li><Link to="#">About</Link></li>
-					<li><Link to="#">Contact Us</Link></li>
-					<li><Link to="#">Gallery</Link></li>
-				</ul>
-				
-				
-			</div>
-		</nav>
-
-
-		)}
-});
-=======
   render:function(){
 
     return(
@@ -71,6 +35,5 @@ render: function(){
 
 })
 
->>>>>>> 3964c37304612cfc314b4fcac0bde8b408d8b923
 
 module.exports = Header;
