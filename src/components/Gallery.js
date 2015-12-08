8307bd0efc61e363672327b07dfada889859a1d6
Arash Dai <arashdai@gmail.com>
2015-12-08 00:51:43 -0800

    basic gallery done

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -6,13 +6,28 @@ var Gallery = React.createClass({
   render:function(){
     return(
       
-      <div className='container-fluid' id='contactpage'>
-
-        <div className='container-fluid Card text-center'>
+      <div className='container-fluid Card text-center' id='contactpage'>
 
         
+          <div className='container-fluid row'>
+             <img id='galleryimage' src='../assets/4333107736_b200fb2fa0_o.jpg' className='img-responsive col-xs-12 col-sm-4 col-md-3 col-lg-2'/>
 
-        </div>
+            <img id='galleryimage' src='../assets/5204325798_f391117172_o.jpg' className='img-responsive col-xs-12 col-sm-4 col-md-3 col-lg-2'/>
+
+            <img id='galleryimage' src='../assets/8541324913_b9a3c0636f_o.jpg' className='img-responsive col-xs-12 col-sm-4 col-md-3 col-lg-2'/>
+
+            <img id='galleryimage' src='../assets/5571576218_3ac3a56b82_o.png' className='img-responsive col-xs-12 col-sm-4 col-md-3 col-lg-2'/>
+           
+            <img id='galleryimage' src='../assets/14366938271_225109c3f4_o.jpg' className='img-responsive col-xs-12 col-sm-4 col-md-3 col-lg-2'/>
+
+            <img id='galleryimage' src='../assets/5237184565_2c769184ac_o.jpg' className='img-responsive col-xs-12 col-sm-4 col-md-3 col-lg-2'/>
+            
+            <img id='galleryimage' src='../assets/17037425094_e002135206_o.jpg' className='img-responsive col-xs-12 col-sm-4 col-md-3 col-lg-2'/>
+
+            <img id='galleryimage' src='../assets/8222566105_7edd7a7394_o.jpg' className='img-responsive col-xs-12 col-sm-4 col-md-3 col-lg-2'/>
+          </div>
+
+      
 
       </div>
   )}
@@ -23,76 +38,3 @@ var Gallery = React.createClass({
 
 module.exports = Gallery;
 
-
-/*
-
-
-
-
-
-
-module.exports = Gallery;
-
-
-var React = require('react');
-
-var img1 = require('.../client/assets/4333107736_b200fb2fa0_o.jpg');
-var img2 = require('.../client/assets/4333107736_b200fb2fa0_o.jpg');
-var img3 = require('.../client/assets/4333107736_b200fb2fa0_o.jpg');
-var img4 = require('.../client/assets/4333107736_b200fb2fa0_o.jpg');
-var img5 = require('.../client/assets/4333107736_b200fb2fa0_o.jpg');
-var img6 = require('.../client/assets/4333107736_b200fb2fa0_o.jpg');
-var img7 = require('.../client/assets/4333107736_b200fb2fa0_o.jpg');
-var img8 = require('.../client/assets/4333107736_b200fb2fa0_o.jpg');
-
-var Data = [img1,img2,img3,img4,img5,img6,img7,img8];
-
-
-var Gallery = React.createClass({
-  loadData:function(){
-    this.setState({
-      images:this.Data
-    })
-  },
-  
- getInitialState: function(){
-    return {
-      images:null
-    };
-  },
-
-  componentDidMount: function() {
-    this.loadData();
-  },
-
-  render:function(){
-    return(
-      
-
-      <div className='container-fluid'>
-
-        <div className='container-fluid Card text-center'>
-        hello
-        
-            
-
-        </div>
-
-      </div>
-  )}
-
-  } );
-
-
-
-module.exports = Gallery;
-
-
-
-
-
-
-
-
-
-*/
