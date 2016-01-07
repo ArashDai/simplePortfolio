83874b8f34602cf62ef8b4da3abbd0d4bd05f515
Arash Dai <arashdai@gmail.com>
2016-01-06 17:29:50 -0800

    asda

--- src/components/Contactus.js
+++ src/components/Contactus.js
@@ -6,15 +6,15 @@ var ContactUs = React.createClass({
 		return(
 			<div className='container-fluid Card text-center' id='contactpage'>
 
-          <strong>Contact Us:</strong>
-          <br></br>
-          Email@website.com
-          <br></br>
-          555 555 5555
-          <br></br>
-          casa@twitter
-          <br></br>
-          casa.facebook
+                         <strong>Contact Us:</strong>
+                         <br></br>
+                         Email@website.com
+                         <br></br>
+                         555 555 5555
+                         <br></br>
+                         casa@twitter
+                         <br></br>
+                         casa.facebook
 
 			</div>
 			
